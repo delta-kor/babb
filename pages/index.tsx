7107db9f37b7f7cc894ff2b40d03b8f6a193368d
delta-kor <deltakbot1@gmail.com>
2020-11-11 19:51:30 +0900

    Add basic flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Index() {
-  return <h1>Index</h1>;
+  const [input, setInput] = useState('');
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>babb</h1>
+      <input type={'text'} onChange={e => setInput(e.target.value)} />
+      <button onClick={() => router.push(`/search?q=${input}`)}>검색</button>
+    </>
+  );
 }
