4c2d519f7ed5c462833785c1716fc31b76161530
delta-kor <deltakbot1@gmail.com>
2020-11-11 19:42:47 +0900

    Add mappings

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,14 +1,33 @@
 import axios from 'axios';
-import { Agent } from 'https';
 import querystring from 'querystring';
+import { Agent } from 'https';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { ApiSearch, ApiSearchItem, SchoolType } from '../../types/api';
 
 const agent = new Agent({
   rejectUnauthorized: false,
 });
 
+function buildItem(source: any, type: SchoolType): ApiSearchItem {
+  return {
+    id: source.SCHUL_CODE,
+    type,
+    name: source.SCHUL_NM,
+    tel: source.USER_TELNO,
+    address: source.SCHUL_RDNMA,
+    homepage: source.HMPG_ADRES,
+  };
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const query = req.query.q as string;
+
+  if (!query.trim()) {
+    const body: ApiSearch = {
+      status: 1,
+    };
+    return res.json(body);
+  }
 
   const form = {
     GS_HANGMOK_CD: '',
@@ -25,5 +44,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     httpsAgent: agent,
   });
 
-  res.json(response.data);
+  const data = response.data;
+
+  let {
+    schoolList02: elementary,
+    schoolList03: middle,
+    schoolList04: high,
+    schoolList05: other,
+  } = data;
+
+  if (!elementary.length && !middle.length && !high.length && !other.length) {
+    const body: ApiSearch = {
+      status: 2,
+    };
+    return res.json(body);
+  }
+
+  elementary = elementary.map(source => buildItem(source, SchoolType.ELEMENTARY));
+  middle = middle.map(source => buildItem(source, SchoolType.MIDDLE));
+  high = high.map(source => buildItem(source, SchoolType.HIGH));
+  other = other.map(source => buildItem(source, SchoolType.OTHER));
+
+  let result: ApiSearchItem[] = [];
+  result = result.concat(elementary, middle, high, other);
+
+  const body: ApiSearch = {
+    status: 0,
+    result,
+  };
+
+  res.json(body);
 }
---
+++ types/api.ts
@@ -0,0 +1,20 @@
+export enum SchoolType {
+  ELEMENTARY,
+  MIDDLE,
+  HIGH,
+  OTHER,
+}
+
+export interface ApiSearch {
+  status: number;
+  result?: ApiSearchItem[];
+}
+
+export interface ApiSearchItem {
+  id: string;
+  type: SchoolType;
+  name: string;
+  tel: string;
+  address: string;
+  homepage: string;
+}
