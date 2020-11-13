d4c73dcc7bf34e9c09cc55b399032b82e1ea8ee5
delta-kor <deltakbot1@gmail.com>
2020-11-13 19:02:44 +0900

    Add main title component

---
+++ components/atoms/MainTitle.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  height: 100px;
+  background: #ffffff;
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  height: 70px;
+  left: 15px;
+  right: 15px;
+  top: calc(50% - 70px / 2);
+  font-style: normal;
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 70px;
+  text-align: center;
+  color: #1287f3;
+`;
+
+interface Props {
+  content: string;
+}
+
+export default class MainTitle extends Component<Props, any> {
+  render() {
+    return (
+      <Layout>
+        <Title>{this.props.content}</Title>
+      </Layout>
+    );
+  }
+}
