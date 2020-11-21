38bcac340c8947ecde514104a2887ecc17645d3d
delta-kor <deltakbot1@gmail.com>
2020-11-21 12:34:48 +0900

    Add api paginating

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -6,6 +6,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   let query = req.query.q as string;
   query = query.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 
+  let page = parseInt(req.query.page as string) - 1 || 0;
+
   if (!query || !query.trim()) {
     const body: ApiSearch = {
       status: 1,
@@ -13,7 +15,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.json(body);
   }
 
-  const documents = await School.find({ name: new RegExp(query, 'gi') }).exec();
+  const documents = await School.find(
+    { name: new RegExp(query, 'gi') },
+    {},
+    { skip: page && 10 * page, limit: 10 }
+  ).exec();
   const result: ApiSearchItem[] = [];
 
   for (const document of documents) {
