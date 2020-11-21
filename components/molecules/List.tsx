da423b0efce054e1ba7484c3e0393fa3c6a6c64a
delta-kor <deltakbot1@gmail.com>
2020-11-21 12:53:18 +0900

    Add list

---
+++ components/molecules/List.tsx
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+import styled from 'styled-components';
+import ListPicker, { ListPickerType } from '../atoms/ListPicker';
+
+const Layout = styled.div`
+  position: relative;
+  height: 51px;
+  background: #ffffff;
+  text-align: center;
+`;
+
+interface Props {
+  total: number;
+  current: number;
+}
+
+export default class List extends Component<Props, any> {
+  render() {
+    const totalDifference = Math.floor(this.props.total / 10) + 1;
+    const difference = Math.floor((this.props.current - 1) / 5);
+    const pickers: JSX.Element[] = [];
+
+    for (let i = 1; i <= 5; i++) {
+      const number = difference * 5 + i;
+      if (number > totalDifference) break;
+      pickers.push(<ListPicker type={ListPickerType.INACTIVE} number={number} key={number} />);
+    }
+
+    return (
+      <Layout>
+        <ListPicker type={ListPickerType.LEFT} />
+        {pickers}
+        <ListPicker type={ListPickerType.RIGHT} />
+      </Layout>
+    );
+  }
+}
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import axios, { AxiosResponse } from 'axios';
@@ -7,6 +7,7 @@ import { ApiSearch } from '../types/api';
 import PageTitle from '../components/atoms/PageTitle';
 import SearchInput from '../components/atoms/Search';
 import SearchItem from '../components/atoms/SearchItem';
+import List from '../components/molecules/List';
 
 const InputWrapper = styled.div`
   padding: 0 24px;
@@ -16,20 +17,23 @@ const SearchItemWrapper = styled.div`
   padding: 33px 0 0 0;
 `;
 
-export default function Search({ query }) {
+export default function Search({ query, page }) {
   const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
+
   const router = useRouter();
 
   useEffect(() => {
     const request: Promise<AxiosResponse<ApiSearch>> = axios.get(
       `${Local.API_ENDPOINT}/api/search`,
       {
-        params: { q: query },
+        params: { q: query, page },
       }
     );
 
     request.then(res => {
       if (res.data.status === 0) {
+        setTotal(res.data.total);
         return setItems(res.data.result.map(item => <SearchItem data={item} key={item.id} />));
       }
       setItems([]);
@@ -43,10 +47,11 @@ export default function Search({ query }) {
         <SearchInput query={query} />
       </InputWrapper>
       <SearchItemWrapper>{items}</SearchItemWrapper>
+      <List total={total} current={page} />
     </>
   );
 }
 
 Search.getInitialProps = ({ query }) => {
-  return { query: query.q };
+  return { query: query.q, page: query.page || 1 };
 };
