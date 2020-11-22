219178fc54e35763e26c7695b49a6012d912ef77
delta-kor <deltakbot1@gmail.com>
2020-11-22 13:52:18 +0900

    Fix web font loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,9 +29,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang={'ko'}>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=swap"
             rel="stylesheet"
           />
         </Head>
