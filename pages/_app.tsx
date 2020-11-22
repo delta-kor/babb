851082bec9b06e92b725f0e20c40b40cac8bd5d3
delta-kor <deltakbot1@gmail.com>
2020-11-22 09:23:17 +0900

    Add meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app';
 import '../styles/global.sass';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
